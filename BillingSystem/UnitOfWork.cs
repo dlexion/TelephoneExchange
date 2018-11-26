bcb5d514e6c836d72cd8b199e6853c90daaa3b39
dlexion <dlexion@yandex.ru>
2018-11-27 02:32:55 +0300

    Create unit of work

---
+++ BillingSystem/UnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using BillingSystem.Repositories;
+
+namespace BillingSystem
+{
+    public class UnitOfWork
+    {
+        public IRepository<Call> Calls { get; }
+
+        public IRepository<Client> Clients { get; }
+    }
+}
